214111b831341c13b10247d724eeec206a2782c7
Daham2000 <dahamakalanka2000@gmail.com>
2022-05-28 13:26:18 +0530

    Content expanded view

--- lib/knowlage_base_page/content_expand_view.dart
+++ lib/knowlage_base_page/content_expand_view.dart
@@ -17,6 +17,13 @@ class ContentExpandView extends StatefulWidget {
 }
 
 class _ContentExpandViewState extends State<ContentExpandView> {
+  final List strings = [
+    "Celestial Navigation",
+    "Terrestrial Navigation",
+    "SQA Formula Sheet",
+    "HNC/D Formula Sheet"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,24 +38,29 @@ class _ContentExpandViewState extends State<ContentExpandView> {
                   text: "Knowledge Base",
                 ),
                 const SizedBox(
-                  height: 20,
+                  height: 25,
                 ),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25.0),
                   child: Container(
                     width: 400,
-                    color: Colors.white,
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
                     child: Column(
                       children: [
-                        Image.network(
-                          "https://media.istockphoto.com/photos/hands-holding-compass-by-sea-with-crashing-waves-picture-id184622521?k=20&m=184622521&s=612x612&w=0&h=Cdu9xyLxx0AuSetLFVzYeJcIGMUe6Yb_lFABBRkCUHQ=",
-                          width: 370,
-                          height: 155,
-                          fit: BoxFit.fill,
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.network(
+                            "https://media.istockphoto.com/photos/hands-holding-compass-by-sea-with-crashing-waves-picture-id184622521?k=20&m=184622521&s=612x612&w=0&h=Cdu9xyLxx0AuSetLFVzYeJcIGMUe6Yb_lFABBRkCUHQ=",
+                            width: 370,
+                            height: 155,
+                            fit: BoxFit.fill,
+                          ),
                         ),
                         ExpandablePanel(
                           header: Padding(
-                            padding: const EdgeInsets.all(8.0),
+                            padding: const EdgeInsets.all(16.0),
                             child: Text(
                               "Navigation",
                               style: GoogleFonts.roboto(
@@ -57,16 +69,86 @@ class _ContentExpandViewState extends State<ContentExpandView> {
                               ),
                             ),
                           ),
-                          collapsed: Text(
-                            "article.body",
-                            softWrap: true,
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
+                          collapsed: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Topics",
+                                  softWrap: true,
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: GoogleFonts.roboto(
+                                    fontSize: 20,
+                                    color: ThemeColors.EXPANED_TEXT_COLOR,
+                                  ),
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    for (final i in strings)
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 10.0),
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            const Divider(),
+                                            Text(
+                                              i,
+                                              softWrap: true,
+                                              maxLines: 1,
+                                              textAlign: TextAlign.justify,
+                                              overflow: TextOverflow.ellipsis,
+                                              style: GoogleFonts.roboto(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold,
+                                                color: ThemeColors
+                                                    .EXPANED_TEXT_COLOR,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      )
+                                  ],
+                                ),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    ElevatedButton(
+                                        onPressed: () {},
+                                        style: ElevatedButton.styleFrom(
+                                          primary: ThemeColors.DOWNLOADBUTTON,
+                                          onPrimary: Colors.white,
+                                          elevation: 0,
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(32.0),
+                                          ),
+                                        ),
+                                        child: Text(
+                                          "Download",
+                                          style: GoogleFonts.roboto(
+                                            fontSize: 15,
+                                            color: Colors.blueAccent,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        )),
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
-                          expanded: Text(
-                            "article.bodyddddddddddddddddddd",
-                            softWrap: true,
-                          ),
+                          expanded: Container(),
                         )
                       ],
                     ),
--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -4,6 +4,7 @@ class ThemeColors {
   static const THEME_COLOR = const Color(0xFF1287B9);
   static const BACKGROUD_COLOR = const Color(0xFFEEEEEE);
   static const BACKGROUD_COLOR_BOTTOM = const Color(0xFF6A6A6A);
+  static const EXPANED_TEXT_COLOR = const Color(0xFF979696);
   static const BLUE_TEXT = const Color(0xFF196F9F);
   static const TEXT_COLOR = const Color(0xFF626262);
   static const TEXT_COLOR_TWO = const Color(0xFF686E71);
@@ -12,4 +13,5 @@ class ThemeColors {
   static const HOMEBUTTONTWO = const Color(0xFF0177FD);
   static const HOMEBUTTONTHREE = const Color(0xFF00CE46);
   static const FORGOT_PASSWORD = const Color(0xFF0F405B);
+  static const DOWNLOADBUTTON = const Color(0xFFE2F5FF);
 }
